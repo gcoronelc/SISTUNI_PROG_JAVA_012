b5b484cb99b32d0886fa37b87367082150c78043
Gustavo Coronel <Gustavo Coronel>
2017-01-12 15:52:57 -0500

    Clase 02

---
+++ Clase02/AprendiendoJava/src/pe/egcc/app/model/Persona.java
@@ -0,0 +1,40 @@
+package pe.egcc.app.model;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class Persona {
+
+  private String nombre;
+  private int edad;
+  private boolean casado;
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getEdad() {
+    return edad;
+  }
+
+  public void setEdad(int edad) {
+    this.edad = edad;
+  }
+
+  public boolean isCasado() {
+    return casado;
+  }
+
+  public void setCasado(boolean casado) {
+    this.casado = casado;
+  }
+  
+  
+  
+  
+}
---
+++ Clase02/PedidoApp/src/pe/egcc/pedidoapp/ClasePrincipal.java
@@ -0,0 +1,16 @@
+package pe.egcc.pedidoapp;
+
+import pe.egcc.pedidoapp.view.PedidoView;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class ClasePrincipal {
+
+  public static void main(String[] args) {
+      System.out.println("Hola todos.");
+      PedidoView.main(args);
+  }
+
+}
